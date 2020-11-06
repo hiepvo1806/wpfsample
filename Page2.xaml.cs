045a960c543050347611c8cf1814be3fd9aafc38
Vo Hoa Hiep <hiep.vo@vn.devinition.com>
2020-11-06 17:39:17 +0700

    add page & navigation

---
+++ Page2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPreviousClick(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService.CanGoBack) NavigationService.GoBack();
+        }
+    }
+}
